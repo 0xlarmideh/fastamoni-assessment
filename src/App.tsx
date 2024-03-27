a93c1f1102a1e33136e3e96e5e09958a2e0419dd
Abdulhameed <busarioyewole@gmail.com>
2024-03-27 10:35:53 +0100

    Added assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 // import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./sections/Header/Header";
+import Navbar from "./sections/Layout/Navbar";
+import NewCaseStudies from "./sections/NewCaseStudies";
+import RealWorldExperience from "./sections/RealWordExperience";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <RealWorldExperience />
+      <NewCaseStudies />
+      <h5 className="bg-black text-white">hsd</h5>
+    </div>
+  );
 }
 
 export default App;
